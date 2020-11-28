a1d399c3fe5e46294a45cfd22a25a44ff0585b35
Kalle Kankare <kalle.kankare@iki.fi>
2020-11-28 17:48:22 +0200

    Add commentary

--- robot-mode.el
+++ robot-mode.el
@@ -27,7 +27,56 @@
 
 ;;; Commentary:
 
-;; TODO
+;; ### Description
+
+;; A Robot Framework major mode for Emacs. Robot Framework is a framework for
+;; acceptance testing.
+
+;; https://robotframework.org
+;; https://robotframework.org/robotframework/latest/RobotFrameworkUserGuide.html
+
+;; This major mode provides the following:
+;; - Syntax highlighting.
+;; - Indentation and alignment.
+;; - Line continuation in Robot Framework syntax.
+
+;; #### Alignment of keyword contents
+
+;; Align the contents of a keyword, test or task with `C-c C-a'. It changes the
+;; following code:
+
+;;     Example Keyword
+;;         [Documentation]    Documents the keyword
+;;         [Arguments]    ${arg1}    ${arg2}
+;;         Log    ${arg1}            ${arg2}
+
+;; To:
+
+;;     Example Keyword
+;;         [Documentation]    Documents the keyword
+;;         [Arguments]        ${arg1}    ${arg2}
+;;         Log                ${arg1}    ${arg2}
+
+;; #### Line continuation
+
+;; Insert a newline, indentation, ellipsis and necessary spaces at current
+;; point with `C-c C-j'. For example (| denotes the cursor):
+
+;;     Another Keyword
+;;         [Documentation]    A very long text| that describes the keyword.
+
+;; To:
+
+;;     Another Keyword
+;;         [Documentation]    A very long text
+;;         ...    | that describes the keyword.
+
+;; ### Limitations
+
+;; - Currently supports only the Space separated format:
+;;   https://robotframework.org/robotframework/latest/RobotFrameworkUserGuide.html#space-separated-format
+;; - Does NOT support the Pipe separated format or the reStructuredText
+;;   format.
 
 ;;; Code:
 
